14d40166c8f837e51366611809a99d3bb84d691d
Oleksii Kukuruza <Oleksii_Kukuruza@epam.com>
2016-09-05 23:37:07 +0300

    Added Google-Maps;

--- client/web/containers/Location.jsx
+++ client/web/containers/Location.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import GoogleMap from 'google-map-react';
 
 export default class Location extends Component {
+  static defaultProps = {
+    center: { lat: 59.938043, lng: 30.337157 },
+    zoom: 9
+  };
+  
   render() {
+    const API_KEY = 'AIzaSyAe5IJKUNflOzXilyKErlps-RlNn-ws0lQ';
     return (
-      <h1>Location</h1>
+      <div style={{ height: '100vh' }}>
+        <GoogleMap
+          bootstrapURLKeys={{
+            key: API_KEY,
+            language: 'ru'
+          }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        />
+      </div>
     );
   }
 }
